58896a6304105d689f55491ec09208a44dab421f
Gani Prayoga <gani.prayoga@gmail.com>
2022-04-26 14:01:39 +0700

    Yarn test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-    apps: [
-        {
-            name: 'TravelApp',
-            exec_mode: 'cluster',
-            instances: 'max',
-            script: './.output/server/index.mjs',
-            env: {
-                "HOST": "0.0.0.0",
-                "PORT": 3000,
-                "NODE_ENV": "development"
-            }
+    apps : [{
+        name      : 'yarn',
+        script    : 'yarn',
+        args      : 'start',
+        interpreter: '/bin/bash',
+        env: {
+            NODE_ENV: 'development'
         }
-    ]
-}
+    }]
+};
