c31580cf3f2f2281ed504f565cf695059c195746
Gani Prayoga <gani.prayoga@gmail.com>
2022-04-26 13:36:15 +0700

    HOST

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,10 +6,12 @@ module.exports = {
             instances: 'max',
             script: './.output/server/index.mjs',
             env: {
+                "HOST": "0.0.0.0",
                 "PORT": 3000,
                 "NODE_ENV": "development"
             },
             env_production: {
+                "HOST": "0.0.0.0",
                 "PORT": 80,
                 "NODE_ENV": "production",
             }
